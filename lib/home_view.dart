70d1119c2035aa32bc1b75d6e72a5eb6abc6ca75
aulafirdaus <aulafirdaus2945@gmail.com>
2024-08-24 14:17:41 +0700

    container: start learn box decoration

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -7,46 +7,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Belajar Flutter'),
+        title: const Text('Container Box Decoration'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Column(
-        children: [
-          Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                margin: const EdgeInsets.only(left: 10, right: 10),
-                padding: const EdgeInsets.all(10), //memberi jarak didalam kotak hijau
-                color: Colors.green,
-                child: Container(
-                  color: Colors.orange,
-                ),
-              ),
-              Container(
-                width: 100, 
-                height: 100,
-                color: Colors.red,
-              )
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.yellow,
-              ),
-              Container(
-                width: 100, 
-                height: 100,
-                color: Colors.purple,
-              )
-            ],
-          ),
-        ],
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          color: Colors.red,
+        ),
       ),
     );
   }
