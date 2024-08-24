acf4cc5440a8c93db1844534a8b72b30db751c07
aulafirdaus <aulafirdaus2945@gmail.com>
2024-08-24 15:24:32 +0700

    Box Decoration: start learning SizedBox

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -7,37 +7,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Container Box Decoration'),
+        title: const Text('Box Decoration | SizedBox'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            // color: Colors.red,
-            border: Border.all(color: Colors.orange, width: 3),
-            // borderRadius: const BorderRadius.only(
-            //   topLeft: Radius.circular(20),
-            //   bottomRight: Radius.circular(20),
-            // ),
-            // gradient: const LinearGradient(
-            //   colors: [Colors.red, Colors.yellow],
-            //   begin: Alignment.topLeft,
-            //   end: Alignment.bottomRight,
-            // ),
-            image: const DecorationImage(
-              image: NetworkImage('https://i.pinimg.com/736x/09/6f/f7/096ff7fd728e880bca931a69a1417a5f.jpg'),
-              fit: BoxFit.cover,
-            ),
-            boxShadow: const [ BoxShadow(
-              color: Colors.orange,
-              blurRadius: 6,
-              spreadRadius: 3,
-              offset: Offset(2, 2),
-            )],
-            shape: BoxShape.circle,
-          ),
+      body: const Center(
+        child: Column(
+          children: [
+            Text('Haiiii.. My Name is Aula Firdaus'),
+            Text('Saat ini saya sedang belajar flutter dengan penuh semangat'),
+          ],
         ),
       ),
     );
