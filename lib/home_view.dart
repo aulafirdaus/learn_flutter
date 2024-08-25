10fc33177456605ed07c3b35b5c9dff29b6e4463
aulafirdaus <aulafirdaus2945@gmail.com>
2024-08-25 21:19:30 +0700

    SizedBox: lebih baik SizedBox daripada Container

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -10,10 +10,17 @@ class HomePage extends StatelessWidget {
         title: const Text('Box Decoration | SizedBox'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
+      // Alasan lebih baik menggunakan SizedBox ketimbang container yaitu untuk menghemat memory yang diload oleh sistem, soalnya SizedBox hanya menerima child lebar dan tinggi, sedangkan container memiliki banyak child yang nantinya akan memakan banyak memory.
       body: const Center(
         child: Column(
           children: [
-            Text('Haiiii.. My Name is Aula Firdaus'),
+            SizedBox(
+              width: 100,
+              // height: 200,
+              child: Text('Haiiii.. My Name is Aula Firdaus, saya adalah seorang developer baik mobile maupun web')),
+              SizedBox(
+                height: 200,
+              ),
             Text('Saat ini saya sedang belajar flutter dengan penuh semangat'),
           ],
         ),
