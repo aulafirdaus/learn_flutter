fcaa96d38ea344deff02d6f78c358ee0780138d8
aulafirdaus <aulafirdaus2945@gmail.com>
2024-08-26 10:05:42 +0700

    Text: start learning Text Widget

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -7,23 +7,11 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Box Decoration | SizedBox'),
+        title: const Text('Learning Widget Text'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      // Alasan lebih baik menggunakan SizedBox ketimbang container yaitu untuk menghemat memory yang diload oleh sistem, soalnya SizedBox hanya menerima child lebar dan tinggi, sedangkan container memiliki banyak child yang nantinya akan memakan banyak memory.
       body: const Center(
-        child: Column(
-          children: [
-            SizedBox(
-              width: 100,
-              // height: 200,
-              child: Text('Haiiii.. My Name is Aula Firdaus, saya adalah seorang developer baik mobile maupun web')),
-              SizedBox(
-                height: 200,
-              ),
-            Text('Saat ini saya sedang belajar flutter dengan penuh semangat'),
-          ],
-        ),
+        child: Text('Hello.. Nama saya Aula Firdaus, saat ini sedang memulai belajar widget text pada flutter'),
       ),
     );
   }
