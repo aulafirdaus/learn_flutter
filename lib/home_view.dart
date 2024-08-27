5620ac379046e5ef8d9e5f683d39a661011b3d23
aulafirdaus <aulafirdaus2945@gmail.com>
2024-08-27 10:30:32 +0700

    start learning change font

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -7,36 +7,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Learning Widget Text'),
+        title: const Text('Belajar Mengubah Font'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: const Center(
         child: SizedBox(
           width: 200,
           child: Text(
-            'Hello.. Nama saya Aula Firdaus, saat ini sedang memulai belajar widget text pada flutter',
-            // textAlign: TextAlign.center,
-            maxLines: 2, //maxLine untuk membatasi baris
-            overflow: TextOverflow.ellipsis, //untuk membuat titik jika tulisannya melebihi batas
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              backgroundColor: Colors.red,
-              fontStyle: FontStyle.italic,
-              shadows: [
-                Shadow(
-                  blurRadius: 5,
-                  color: Colors.yellow,
-                  offset: Offset(2, 2),
-                ),
-              ],
-              letterSpacing: 2, //memberikan jarak per huruf nya
-              wordSpacing: 2, //memberikan jarak per kata
-              decoration: TextDecoration.underline,
-              decorationColor: Colors.blue,
-              decorationStyle: TextDecorationStyle.wavy, //memberikan underline bergelombang
-              decorationThickness: 2,
-            ),
+            'Hello.. Nama saya Aula Firdaus, saat ini sedang memulai belajar mengubah font pada flutter',
           ),
         ),
       ),
