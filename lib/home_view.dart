7b7b2404b060ec941d9e5f95333b9508e5b071db
aulafirdaus <aulafirdaus2945@gmail.com>
2024-08-28 11:26:24 +0700

    wrap: belajar wrap untuk tataletak/responsif part 1

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -10,7 +10,32 @@ class HomePage extends StatelessWidget {
         title: const Text('Layout | Wrap Widget'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const SizedBox(
+      body: Container(
+        color: Colors.grey.shade200,
+        width: double.infinity, //infinity untuk menyesuikan container dengan ukuran layar device
+        height: double.infinity,
+        child: Wrap(
+          // Wrap adalah widget untuk membuat layout yang dinamis, fleksibel dan responsif menyesuaikan ukuran layar device.
+          direction: Axis.horizontal, // untuk mengubah urutan widget, secara default urutannya horizontal(dari kiri ke kakan).
+          spacing: 10, //untuk memberikan jarak secara horizontal
+          runSpacing: 20, //untuk memberikan jarak secara vertikal
+          runAlignment: WrapAlignment.end, //runAlignment untuk menentukan posisi awal dari alignment, jika runAlignment nya "start" berarti alignment nya menempel diatas, begitu sebaliknya.
+          alignment: WrapAlignment.end, //wrapalignment untuk mengatur posisi index, jika "end" index nya menempel di kanan, jika "start" menempel di kiri, jika "center" berarti menempel diatas/tengah.
+          children: List.generate(8, (index) => Container(
+            width: 100,
+            height: 100,
+            color: index % 2 == 0 ? Colors.green : Colors.red, //jika indexContainer habis dibagi 2 maka warna hijau jika tidak warna merah
+            child: Center(
+              child: Text(index.toString(),
+              style: const TextStyle(
+                color: Colors.white, 
+                fontSize: 50,
+                ),
+              ),
+            ),
+          ),
+        ),
+        ),
       ),
     );
   }
