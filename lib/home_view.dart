e2b32d06117c347726ea051369c5ec042acb995b
aulafirdaus <aulafirdaus2945@gmail.com>
2024-08-28 12:40:18 +0700

    start learning padding widget

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -7,16 +7,17 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Align Widget'),
+        title: const Text('Padding Widget'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Align(
-        // alignment: Alignment.center, //alignment bisa menggunakan widget ini atau bisa dengan cara kedua yang dibawah ini
-        alignment: Alignment(-1, -1.1), //cara kedua dengan memberikan nilai x dan y dengan nilai koma maupun biasa
-        child: SizedBox(
-          width: 300,
-          child: Text('Hallo saat ini saya sedang belajar Align Widget'),
-        ),
+      body: const Row(
+        children: [
+          Text('Hallo'),
+          Text('Nama'),
+          Text('Saya'),
+          Text('Aula'),
+          Text('Firdaus'),
+        ],
       ),
     );
   }
