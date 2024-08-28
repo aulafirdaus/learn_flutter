ca520221bd92becbcaa98ec5c0036c48ab057f1a
aulafirdaus <aulafirdaus2945@gmail.com>
2024-08-28 10:49:33 +0700

    start learning wrap widget

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -7,35 +7,10 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Layout | Stack Widget'),
+        title: const Text('Layout | Wrap Widget'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Container(
-        // Stack adalah widget yang digunakan untuk menempatkan widget di dalam widget lainnya dan mengatur posisi widgetnya
-        color: Colors.yellow.shade100,
-        child: Stack(
-          alignment: Alignment.topRight, // mengubar posisi widget yang didalam stack
-          children: [
-            Container(
-              margin: const EdgeInsets.all(10),
-              width: 100,
-              height: 100,
-              color: Colors.red,
-            ),
-            Container(
-              margin: const EdgeInsets.all(30),
-              width: 100,
-              height: 100,
-              color: Colors.green,
-            ),
-            Container(
-              margin: const EdgeInsets.all(50),
-              width: 100,
-              height: 100,
-              color: Colors.blue,
-            ),
-          ],
-        ),
+      body: const SizedBox(
       ),
     );
   }
