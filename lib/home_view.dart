c211b8bc7e3a438bfcd9fa5acbe3d58080f748f9
aulafirdaus <aulafirdaus2945@gmail.com>
2024-08-28 11:40:01 +0700

    wrap: belajar wrap untuk tataletak/responsif part 2

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -16,14 +16,16 @@ class HomePage extends StatelessWidget {
         height: double.infinity,
         child: Wrap(
           // Wrap adalah widget untuk membuat layout yang dinamis, fleksibel dan responsif menyesuaikan ukuran layar device.
-          direction: Axis.horizontal, // untuk mengubah urutan widget, secara default urutannya horizontal(dari kiri ke kakan).
+          // direction: Axis.horizontal, // untuk mengubah urutan widget, secara default urutannya horizontal(dari kiri ke kakan).
           spacing: 10, //untuk memberikan jarak secara horizontal
           runSpacing: 20, //untuk memberikan jarak secara vertikal
-          runAlignment: WrapAlignment.end, //runAlignment untuk menentukan posisi awal dari alignment, jika runAlignment nya "start" berarti alignment nya menempel diatas, begitu sebaliknya.
-          alignment: WrapAlignment.end, //wrapalignment untuk mengatur posisi index, jika "end" index nya menempel di kanan, jika "start" menempel di kiri, jika "center" berarti menempel diatas/tengah.
-          children: List.generate(8, (index) => Container(
-            width: 100,
-            height: 100,
+          crossAxisAlignment: WrapCrossAlignment.center, //crossAxisAlignment untuk mengatur tataletak index/container nya, default nya "start" itu baris atas nya lurus dengan index/container lainnya. crossAxisAlignment untuk mengatur tataletak PER widget/index/container nya.
+          runAlignment: WrapAlignment.end, //runAlignment untuk menentukan posisi awal dari alignment, jika runAlignment nya "start" berarti alignment nya menempel diatas, begitu sebaliknya. runAlignment untuk mengatur tataletak KUMPULAN widget/index/container nya.
+          // alignment: WrapAlignment.end, //wrapalignment untuk mengatur posisi index, jika "end" index nya menempel di kanan, jika "start" menempel di kiri, jika "center" berarti menempel diatas/tengah.
+          verticalDirection: VerticalDirection.up, //jika VerticalDirection nya up, angka 0 nya dimulai dari bawah, jika down angka 0 nya dimulai dari atas.
+          children: List.generate(9, (index) => Container(
+            width: index % 2 == 0 ? 100 : 80,
+            height: index % 2 == 0 ? 100 : 80,
             color: index % 2 == 0 ? Colors.green : Colors.red, //jika indexContainer habis dibagi 2 maka warna hijau jika tidak warna merah
             child: Center(
               child: Text(index.toString(),
