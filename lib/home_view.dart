5813b61d8d569b6d3b5212dc2ad37cba3b7c23fd
aulafirdaus <aulafirdaus2945@gmail.com>
2024-08-29 10:26:44 +0700

    start learning Icon Widget

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -7,28 +7,10 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Padding Widget'),
+        title: const Center(child: Text('Icon Widget')),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Row(
-        children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 20, 10, 0),
-            child: Text('Hallo')),
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 20, 10, 0),
-            child: Text('Nama')),
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 20, 10, 0),
-            child: Text('Saya')),
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 20, 10, 0),
-            child: Text('Aula')),
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 20, 10, 0),
-            child: Text('Firdaus')),
-        ],
-      ),
+      body: const Center(),
     );
   }
 }
