90ca828a23d01d264cf28c75e5cf590b854f801b
aulafirdaus <aulafirdaus2945@gmail.com>
2024-08-29 11:14:26 +0700

    TextField: membuat form inputan dengan tampilan seperti password

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -11,8 +11,20 @@ class HomePage extends StatelessWidget {
         title: const Center(child: Text('TextField Widget')),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              obscureText: true, // obscureText untuk memberikan inputnya menjadi bulat seperti type password yang ada pada web
+              obscuringCharacter: '*', //obscuringCharacter untuk mengganti karakter yang ditampilkankan pada inputan, jika di isi "*" maka inputan diubah menjadi tanda bintang
+              decoration: InputDecoration(
+                icon: Icon(MdiIcons.lock),
+                labelText: 'Password',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
